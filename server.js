4286541971f789746b8d0ffb75cc742f52d87010
Antti Keränen <detegr@gmail.com>
2013-09-13 08:30:16 +0300

    Very simple uploading support

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express=require("express")
+var express=require("express"),
+	format=require("util").format;
 var app=express()
+app.use(express.bodyParser()) // For parsing multipart form data
 
 app.get("/", function(req, res, next)
 {
 	res.type("text/plain");
 	res.send("hello world");
+});
+
+app.get("/upload", function(req, res, next)
+{// For debugging
+	res.send("<form method='post' enctype='multipart/form-data'><input type='file' name='video'/>"
+		    +"<input type='submit' value='upload'/></form>");
+});
+
+app.post("/upload", function(req, res, next)
+{
+	res.send(format("\n uploaded %s (%d Kb) to %s"
+		, req.files.video.name
+		, req.files.video.size / 1024 | 0
+		, req.files.video.path));
 });
 
 app.listen(9999);
